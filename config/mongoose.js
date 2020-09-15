5f2cf7e094b0d6e15bd73e2c3fc2c7a9e4adf9c5
Sonu <sonu3660@gmail.com>
2020-09-15 14:51:08 +0530

    Add mongoose as database

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/Resolute_AI");
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error Connection to MongoDB"));
+
+db.once("open", function () {
+   console.log("Connected to Database :: MongoBD");
+});
+
+module.exports = db;
