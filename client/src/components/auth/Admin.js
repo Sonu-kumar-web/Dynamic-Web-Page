a8a7252b27036d9b33d854e9c7debe44dcf3e963
Sonu <sonu3660@gmail.com>
2020-09-17 01:40:28 +0530

    Add login method

--- client/src/components/auth/Admin.js
+++ client/src/components/auth/Admin.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Admin = () => {
-   return <div>Admin</div>;
+   return (
+      <section className="container Sign-up">
+         <h1 className="large text-primary">Admin Login</h1>
+         <p className="lead">
+            <i className="fas fa-user"></i> Sign into Admin Account
+         </p>
+         <form className="form" action="dashboard.html">
+            <div class="form-group">
+               <input
+                  type="email"
+                  placeholder="Email Address"
+                  name="email"
+                  required
+               />
+            </div>
+            <div className="form-group">
+               <input type="password" placeholder="Password" name="password" />
+            </div>
+            <input type="submit" class="btn btn-primary" value="Login" />
+         </form>
+      </section>
+   );
 };
 
 export default Admin;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,33 +2,73 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "../../App.css";
+import axios from "axios";
 
-export const Login = () => {
-   return (
-      <section class="container Sign-up">
-         <h1 class="large text-primary">Sign In</h1>
-         <p class="lead">
-            <i class="fas fa-user"></i> Sign into Your Account
-         </p>
-         <form class="form" action="dashboard.html">
-            <div class="form-group">
-               <input
-                  type="email"
-                  placeholder="Email Address"
-                  name="email"
-                  required
-               />
-            </div>
-            <div class="form-group">
-               <input type="password" placeholder="Password" name="password" />
-            </div>
-            <input type="submit" class="btn btn-primary" value="Login" />
-         </form>
-         <p class="my-1">
-            Don't have an account? <Link to="/register">Sign Up</Link>
-         </p>
-      </section>
-   );
-};
+class Login extends React.Component {
+   state = { email: "", password: "", token: "" };
+
+   onSubmit = async (e) => {
+      e.preventDefault();
+
+      // console.log("State", this.state);
+
+      let userData = {
+         email: this.state.email,
+         password: this.state.password,
+      };
+
+      try {
+         let data = await axios.post("api/v1/user/login", userData);
+
+         const { token } = data.data.data;
+         console.log("Token", token);
+
+         // Save token in local storage
+         localStorage.setItem("jwtToken", token);
+      } catch (err) {
+         console.log("Error In login ", err);
+      }
+   };
+
+   render() {
+      const { email, password } = this.state;
+      return (
+         <section className="container Sign-up">
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+               <i className="fas fa-user"></i> Sign into Your Account
+            </p>
+            <form className="form" onSubmit={(e) => this.onSubmit(e)}>
+               <div className="form-group">
+                  <input
+                     type="email"
+                     placeholder="Email Address"
+                     name="email"
+                     value={email}
+                     onChange={(e) => this.setState({ email: e.target.value })}
+                     required
+                  />
+               </div>
+               <div className="form-group">
+                  <input
+                     type="password"
+                     placeholder="Password"
+                     name="password"
+                     value={password}
+                     onChange={(e) =>
+                        this.setState({ password: e.target.value })
+                     }
+                     required
+                  />
+               </div>
+               <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+               Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+         </section>
+      );
+   }
+}
 
 export default Login;
